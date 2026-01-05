3dbacad8aa948d9262a0f2e38473b68fd85dd875
Grussniffer <126242825+Grussniffer@users.noreply.github.com>
2026-01-05 14:04:28 +0100

    Add application configuration for Supabase

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://supabase.grusmedia.no:444",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTc0OTU5MzQ2NiwiZXhwIjoyMDY0OTUzNDY2fQ.Eq_oqn_miVnHoQGO1gbZJQgmonJRAxv7NQRgoWg5Z_Q"
+};
